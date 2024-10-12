37990df19d12e05175eca7573e1cf2f793aee2d2
Regarddeb <unohumphrey260@gmail.com>
2024-10-12 13:06:47 +0800

    Add auth restriction to add / update blog template view

--- blog/views.py
+++ blog/views.py
@@ -14,11 +14,11 @@ from .serializers.BlogRequestSerializer import *
 class BlogPostListView(TemplateView):
     template_name = 'blogs/my_blogs.html'
 
-class BlogPostCreateView(TemplateView):
+class BlogPostCreateView(LoginRequiredMixin, TemplateView):
     template_name = 'blogs/create_blog_form.html'
     
     
-class BlogPostEditView(TemplateView):
+class BlogPostEditView(LoginRequiredMixin, TemplateView):
     template_name = 'blogs/edit_blog_form.html'
 
     def get_context_data(self, **kwargs):
